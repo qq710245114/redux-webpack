e0e3e5b6abc71344e6141f77b75a6bf0ce7b986d
zhengyingxian <710245114@qq.com>
2017-06-17 16:04:50 +0800

    123

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,29 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/, // 用正则来匹配文件路径，这段意思是匹配 js 或者 jsx
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['react', 'es2015'],
+            },
+            include: __dirname
+        },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(jpg|png)$/,
+                loader: 'url-loader?limit=25000'
+            }
+        ]
+    }
 };
